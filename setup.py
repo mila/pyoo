97b28581c076fe2710a635e839ea220bf68b91aa
Miloslav Pojman <miloslav.pojman@gmail.com>
2017-12-05 22:08:25 +0100

    Increment version after release.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='pyoo',
-    version='1.3',
+    version='1.4.dev',
     description='Pythonic interface to Apache OpenOffice API (UNO)',
     long_description = open('README.rst').read(),
     author='Miloslav Pojman',
